9a7d7b8c9232328f97b82d558ec89f49f31360bc
weihb2002 <zengzy02@gmail.com>
2019-03-23 09:46:56 -0700

    zz change

---
+++ zz-js file/zz.js
@@ -0,0 +1,9 @@
+// fetch info from trivia api
+
+
+function submit(){
+    fetch('https://opentdb.com/api.php?amount=10&category=9')
+      .then(function(response) {
+        return response.json();
+      }).then(function(myJson) {}
+           
